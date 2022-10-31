db0bc8728717867c729ba32852259209f3e36bc0
Sara <sara.al-arab@logivations.com>
2022-10-31 18:56:28 +0200

    wip

--- src/components/Classifiers/Classifiers.js
+++ src/components/Classifiers/Classifiers.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
-import {backend} from "../../config"
+import React, { Component , useEffect, useState} from 'react';
 
-class Classifiers extends Component {
- componentDidMount() {
- // Runs after the first render() lifecycle
- console.log("Did mount called");
- fetch(`${backend}/info`).then((result) => {
-    if (result.ok) {
-        result.json().then((body) => {
-            console.log(body);
-        });
-    }
-});
+import {backend} from "../../config"
+import Classifier from '../Classifier/Classifier';
+
+
+function Classifiers(props) {
+
+const [models, setModels] = useState([])
+
+function getModels() {
+let models_1 = []
+// Runs after the first render() lifecycle
+console.log("Did mount called");
+models_1 = fetch(`${backend}/info`).then((result) => {
+if (result.ok) {
+    result.json().then((body) => {
+        setModels(body);
+        
+    });
 }
 
-render() {
- console.log('Render lifecycle')
- return <h1>Hello World!</h1>;
- }
+});
+return models_1;
+}
+
+useEffect(() => {
+    getModels()
+}, [models])
+
+ return (
+    <div>
+        Hello
+        {/* {models.map((item,index)=>{
+
+        return <Classifier key={index} />
+
+        })} */}
+    </div>
+ );
+ 
 }
 export default Classifiers;  
