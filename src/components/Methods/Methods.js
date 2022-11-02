41d53aedf2660376be7590e42de3b2052913a2e2
Sara <sara.al-arab@logivations.com>
2022-11-02 18:30:01 +0200

    .

--- src/components/Methods/Methods.js
+++ src/components/Methods/Methods.js
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Card from "@material-ui/core/Card";
-import { Button } from "@material-ui/core";
 import { backend } from "../../config";
-import {predict, subscribeToPrediction} from "../API/BackendCalls";
-import { Height } from "@material-ui/icons";
 import "../Methods/Methods.css";
 
-function Methods(props) {
+function Methods({ chooseImage }) {
 
 
   const [image, setImage] = useState();
-  const [prediction, setPrediction] = useState("");
-  const [isPredictOn, setPredict] = useState("");
 
   const hiddenFileInput = React.useRef(null);
   
@@ -22,63 +16,6 @@ function Methods(props) {
   const handleClick = (event) => {
     hiddenFileInput.current.click();
   };
-
-  const handlePredictionResult = (label) => {
-      setPrediction(label);
-  };
-
-  subscribeToPrediction(handlePredictionResult);
-
-  async function handlePredict(e) {
-    console.log("sending prediction request!");
-    console.log(image);
-
-    let reader = new FileReader();
-    let blob = await fetch(image).then((r) => r.blob());
-    console.log(blob);
-    reader.readAsDataURL(blob);
-    reader.onload = function () {
-      console.log(reader.result);
-      URL.revokeObjectURL(image);
-      setImage();
-
-      const req = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ image: reader.result }),
-      };
-
-      const body = {image: reader.result}
-
-      // predict(body)
-
-      // console.log(`${backend}/predict`);
-      fetch(`${backend}/predict`, req).then((postRes) => {
-        // we now get the id, and fetch the result in 0.5s
-
-        if (postRes.ok) {
-          postRes.json().then((body) => {
-            const jobId = body.jobId;
-
-            let timer = setInterval(() => {
-              fetch(`${backend}/jobinfo?jobId=${jobId}`).then((predRes) => {
-                if (predRes.ok) {
-                  predRes.json().then((bodyPred) => {
-                    console.log(bodyPred);
-                    if ("label" in bodyPred.jobResults) {
-                      console.log("inside IF");
-                      setPrediction(bodyPred.jobResults.label);
-                      clearInterval(timer)
-                    }
-                  });
-                }
-              });
-            }, 1000);
-          });
-        }
-      });
-    };
-  }
 
   return (
     <div>
@@ -92,17 +29,15 @@ function Methods(props) {
             console.log("GOT A FILE!");
             console.log(hiddenFileInput.current.files[0].name);
             if (typeof image != "undefined") URL.revokeObjectURL(image);
-            setPrediction("");
             setImage(URL.createObjectURL(hiddenFileInput.current.files[0]));
+            chooseImage(URL.createObjectURL(hiddenFileInput.current.files[0]))
           }}
         />
         <button onClick={handleClick} className="btn btn-primary button" style={{marginRight: 15, fontSize: '20px'}}>Browse</button>
         <button className="btn btn-warning button" style={{fontSize: '20px'}}>Draw</button>
     </div>
     <div style={{alignContent: 'center', display: 'flex', justifyContent: 'center', marginTop: 20}}>
-    {image === undefined? 
-        '':
-            <div className="">
+      {image === undefined? '': <div className="">
         <img className="myImage" style={{width: '300px', height: '300px'}} src={image} alt="" />
       </div>
         }
--- src/pages/Predict.jsx
+++ src/pages/Predict.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
 import Classifiers from '../components/Classifiers/Classifiers'
 import Methods from '../components/Methods/Methods';
+import { backend } from '../config';
 
 function Predict() {
+
+
+  const [image, setImage] = React.useState();  
+  const chooseImage = (message) => {
+
+    setImage(message);
+  };
+
+  async function handlePredict(e) {
+ 
+    let reader = new FileReader();
+    let blob = await fetch(image).then((r) => r.blob());
+    console.log(blob);
+    reader.readAsDataURL(blob);
+    reader.onload = function () {
+      console.log(reader.result);
+      // URL.revokeObjectURL(image);
+      setImage();
+
+      const req = {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin":"*" },
+        body: JSON.stringify({ image: reader.result }),
+      };
+
+      fetch(`${backend}/predict`, req).then((postRes) => {
+        // we now get the id, and fetch the result in 0.5s
+        console.log("FETCHED")
+        if (postRes.ok) {
+          postRes.json().then((body) => {
+            const jobId = body.jobId;
+
+            let timer = setInterval(() => {
+              fetch(`${backend}/jobinfo?jobId=${jobId}`).then((predRes) => {
+                if (predRes.ok) {
+                  predRes.json().then((bodyPred) => {
+                    console.log(bodyPred);
+                    if ("label" in bodyPred.jobResults) {
+                      console.log("inside IF");
+                      clearInterval(timer)
+                    }
+                  });
+                }
+              });
+            }, 1000);
+          });
+        }
+      });
+    };
+  }
+
+
+
   return (
     <div className="app">
       <Classifiers/>
-      <Methods/>
+      <Methods chooseImage={chooseImage}/>
+      <button onClick={handlePredict} className="btn btn-secondary button" style={{marginLeft:'45%' , fontSize: '30px'}}>Predict</button>
   </div>
   );
 }
